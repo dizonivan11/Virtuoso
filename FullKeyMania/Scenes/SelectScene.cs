23e4effcf8961b49f1aaec7c4420406787ee9d6d
dizonivan11 <dizonivan11@gmail.com>
2023-04-06 20:15:56 +0800

    Added Song Selection Scene

---
+++ FullKeyMania/Scenes/SelectScene.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullKeyMania.Scenes {
+    internal class SelectScene : Scene {
+        public SelectScene(MainScene main) : base(main) {
+
+        }
+
+        internal override void Update(
+            GameTime gameTime,
+            KeyboardState previousKeyState,
+            KeyboardState currentKeyState,
+            MouseState previousMouseState,
+            MouseState currentMouseState) {
+            
+        }
+
+        internal override void Draw() {
+            
+        }
+    }
+}
