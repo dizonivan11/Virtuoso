7aec361aaf0867ba126d3d2c6c5fde1d4d1f407f
dizonivan11 <dizonivan11@gmail.com>
2023-06-14 04:54:45 +0800

    Added Visual Debug

--- FullKeyMania/Components/GameObjects/GameObject.cs
+++ FullKeyMania/Components/GameObjects/GameObject.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
+using MonoGame.Forms.Controls;
 
 namespace FullKeyMania.Components.GameObjects {
     public abstract class GameObject {
-        internal abstract void Update(GameTime gameTime, InputState input);
+        internal abstract void Update(GameTime gameTime, InputState input, GraphicsDevice graphics);
         internal abstract void Draw(SpriteBatch spriteBatch);
+        internal virtual void DrawDebug(SpriteBatch spriteBatch, Texture2D pixel) { }
     }
 }
