66c0f206604ef368978dba2b5530abfcf2fa17a6
dizonivan11 <dizonivan11@gmail.com>
2023-08-27 00:30:32 +0800

    Initialized scene

--- FullKeyMania/Scenes/SelectScene.cs
+++ FullKeyMania/Scenes/SelectScene.cs
@@ -1,24 +1,38 @@
 ﻿using FullKeyMania.Components;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Input;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace FullKeyMania.Scenes {
     internal class SelectScene : Scene {
-        public SelectScene(MainScene main) : base(main) {
+        public const int CARD_HEIGHT = 128;
+        public const int CARD_PADDING = 8;
 
+        readonly List<Beatmap> beatmaps;
+
+        public SelectScene(MainScene main) : base(main) {
+            beatmaps = new List<Beatmap>();
+            LoadAllBeatmaps();
+        }
+
+        private void LoadAllBeatmaps() {
+            beatmaps.Clear();
+            string[] songDirs = Directory.GetDirectories("songs");
+            foreach (string songDir in songDirs) {
+                beatmaps.Add(new Beatmap(songDir));
+            }
         }
 
         internal override void Update(GameTime gameTime, InputState input) {
-            
+
         }
 
         internal override void Draw() {
-            
+            Vector2 cardPos = new Vector2(0, CARD_PADDING);
+            foreach (var beatmap in beatmaps) {
+                MainScene.Editor.spriteBatch.DrawString(MainScene.Editor.Font, beatmap.Name, cardPos, Color.White);
+                cardPos.Y += CARD_HEIGHT + CARD_PADDING;
+            }
         }
     }
 }
